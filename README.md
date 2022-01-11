ca89e3c98f462baa9eff20556cbcfae63693c04a
BowoTETEW <rikkyagung11@gmail.com>
2022-01-11 12:09:18 +0700

    Done

---
+++ README.md
@@ -0,0 +1,16 @@
+# Portfolio Website - Bootstrap v5 and SCSS
+This is my first portfolio page. This website very simple and responsive.
+![homepage](/assets/img/project-2.png)
+
+## Color Pallete
+ Color         | Hex           
+ ------------- |-------------
+ Primary       | ![#6266F5](https://via.placeholder.com/15/6266F5/000000?text=+) `#6266F5`
+ Primary Dark  | ![#2B2C46](https://via.placeholder.com/15/2B2C46/000000?text=+) `#2B2C46`
+ Primary Darker| ![#27293F](https://via.placeholder.com/15/27293F/000000?text=+) `#27293F`
+ Secondary     | ![#282736](https://via.placeholder.com/15/282736/000000?text=+) `#282736`
+
+## Source
+- [Bootstrap](https://getbootstrap.com/)
+- [FontAwesome](https://fontawesome.com/)
+- [AOS](https://michalsnik.github.io/aos/)
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+// Hamburger Menu
 const menu = document.querySelector('.hamburger-menu');
 const nav = document.querySelector('.menu ul')
 
@@ -10,15 +11,31 @@ menu.addEventListener('click', () => {
 });
 
 
-
+// Animate Fixed Navbar
 const navbar = document.querySelector('.menu');
 
 function scrollTop() {
     const scroll = window.scrollY;
     if (scroll > 70) {
-        navbar.classList.add('sticky')
+        navbar.classList.add('fixed')
     } else {
-        navbar.classList.remove('sticky')
+        navbar.classList.remove('fixed')
     }
 }
 window.addEventListener('scroll', scrollTop);
+
+
+// Animate Skills Icons Card AOS
+const card = document.querySelectorAll('.icon-card');
+
+card.forEach((icon, i) => {
+    icon.dataset.aos = 'fade-up';
+    icon.dataset.aosDelay = i * 100;
+});
+
+
+// AOS
+AOS.init({
+    once: true,
+    duration: 1000
+});
